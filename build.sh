e6bc99f97fca8a5aa023ae2c382a0b7b74045adb
Artiom Astashonak <artiomastashonak@gmail.com>
2025-05-11 12:31:03 +0200

    Update build script.

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 BIN=bin
+TARGET=jgame
+export BACKEND_DEV=1
 
 backend() {
+  if [[ $BACKEND_DEV = 1 ]]; then echo "Building backend in development mode...";
+  else echo "Building backend in production mode..."; fi
   mkdir -p $BIN
-  go build -o ./$BIN/jgame ./cmd/jgame
+  go build -o ./$BIN/$TARGET ./cmd/$TARGET
 }
 
 frontend() {
@@ -16,18 +20,41 @@ all() {
   frontend
 }
 
+clean() {
+  rm -rf $BIN
+  rm -rf ./website/dist
+}
+
 case "$1" in
+  run)
+    if [ -z "$2" ]; then
+      echo "No argument after *run* command."
+      exit 1
+    elif [ "$2" = "back" ]; then
+      backend
+      ./$BIN/$TARGET
+    else
+      echo "Unknown application *${2}*"
+      exit 1
+    fi
+    ;;
   back)
     backend
     ;;
   front)
+    echo "Compiling frontend into static files..."
     frontend
+    ;;
+  clean)
+    echo "Removing build directories..."
+    clean
     ;;
   *)
     if [ -z "$1" ]; then
+      BACKEND_DEV=0
       all
     else
-      echo "Unknown command ${1}"
+      echo "Unknown command *${1}*"
     fi
     ;;
 esac
