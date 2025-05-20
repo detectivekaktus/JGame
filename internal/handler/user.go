fad338132aa8766501e065b7caee839872ddf1c9
Artiom Astashonak <artiomastashonak@gmail.com>
2025-05-20 13:50:32 +0200

    Add PATCH method to `/api/users/{id}` endpoint.

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/detectivekaktus/JGame/internal/database"
@@ -224,7 +225,6 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-// TODO: Make a PATCH alternative.
 func PutUser(w http.ResponseWriter, r *http.Request) {
 	if !httputil.IsContentType(w, r, "application/json") {
 		httputil.SendErrorMessage(w, http.StatusBadRequest, "Content-Type mismatch",
@@ -267,11 +267,11 @@ func PutUser(w http.ResponseWriter, r *http.Request) {
 
 	if user.Id != 0 {
 		httputil.SendErrorMessage(w, http.StatusBadRequest, "Malformatted request",
-			"Can't modify an id of a user.")
+			"Can't modify id of a user.")
 		return
 	}
 
-	if user.Name == "" || user.Email == "" || user.Password == "" {
+	if strings.TrimSpace(user.Name) == "" || strings.TrimSpace(user.Email) == "" || strings.TrimSpace(user.Password) == "" {
 		httputil.SendErrorMessage(w, http.StatusBadRequest, "Missing fields",
 			"name, email, and password fields must be specified on PUT request.")
 		return
@@ -295,5 +295,101 @@ func PutUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func PatchUser(w http.ResponseWriter, r *http.Request) {
+	if !httputil.IsContentType(w, r, "application/json") {
+		httputil.SendErrorMessage(w, http.StatusBadRequest, "Content-Type mismatch",
+			"Expected Content-Type to be application/json.")
+		return
+	}
+
+	if !httputil.HasContent(r) {
+		httputil.SendErrorMessage(w, http.StatusBadRequest, "No content provided",
+			"Expected content inside the request body, got nothing.")
+		return
+	}
+
+	conn := database.GetConnection()
+	defer conn.Close(context.Background())
+
+	session, _ := getUserSession(conn, r)
+	if session == nil {
+		httputil.SendErrorMessage(w, http.StatusUnauthorized, "Unauthorized",
+			"Can't update user when not logged in.")
+		return
+	}
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if int_id, _ := strconv.Atoi(id); session.UserId != int_id {
+		httputil.SendErrorMessage(w, http.StatusForbidden, "Forbidden",
+			"Can't update user that is not themselves.")
+		return
+	}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		httputil.SendErrorMessage(w, http.StatusBadRequest, "Malformatted request",
+			"Could not process the body of the request.")
+		return
+	}
+
+	if user.Id != 0 {
+		httputil.SendErrorMessage(w, http.StatusBadRequest, "Malformatted request",
+			"Can't modify id of a user.")
+		return
+	}
+
+	var fieldsSb strings.Builder
+	// Even though I know that all the values inside args are of type string
+	// I still need to define it as []any, so I can use it later on on database.Execute
+	var args []any
+	fieldsSb.WriteString("UPDATE users.\"user\" SET ")
+
+	if strings.TrimSpace(user.Email) != "" {
+		args = append(args, user.Email)
+		fieldsSb.WriteString(fmt.Sprintf("email = $%d", len(args)))
+	}
+
+	if strings.TrimSpace(user.Name) != "" {
+		if len(args) != 0 {
+			fieldsSb.WriteString(", ")
+		}
+		args = append(args, user.Name)
+		fieldsSb.WriteString(fmt.Sprintf("name = $%d", len(args)))
+	}
+
+	if strings.TrimSpace(user.Password) != "" {
+		if len(args) != 0 {
+			fieldsSb.WriteString(", ")
+		}
+
+		hash, err := hashPassword(user.Password)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not generate password hash for PATCH /api/users/id: %v\n", err)
+			httputil.SendErrorMessage(w, http.StatusInternalServerError, "Internal error",
+				"Could not process user password.")
+			return
+		}
+		user.Password = hash
+
+		args = append(args, user.Password)
+		fieldsSb.WriteString(fmt.Sprintf("password = $%d", len(args)))
+	}
+	args = append(args, id)
+	fieldsSb.WriteString(fmt.Sprintf(" WHERE user_id = $%d", len(args)))
+
+	_, err = database.Execute(conn, fieldsSb.String(), args...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not update user for PATCH /api/users/id: %v\n", err)
+		httputil.SendErrorMessage(w, http.StatusInternalServerError, "Internal error",
+			"Could not update user.")
+		return
+	}
+	
 	w.WriteHeader(http.StatusNoContent)
 }
