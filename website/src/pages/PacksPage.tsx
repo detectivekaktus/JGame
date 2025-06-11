3f6df54e4eb5baa85d27834dac2007335c2232aa
Artiom Astashonak <artiomastashonak@gmail.com>
2025-06-11 13:20:56 +0200

    Implement search bar on packs page.

--- website/src/pages/PacksPage.tsx
+++ website/src/pages/PacksPage.tsx
@@ -3,10 +3,12 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Pack, PackCard } from "../components/PackCard";
 import { BASE_API_URL } from "../utils/consts";
+import { Button } from "../components/Button";
+import { Spinner } from "../components/Spinner";
 import "../css/PacksPage.css"
-import { LoadingPage } from "./LoadingPage";
 
 export function PacksPage() {
+  const [query, setQuery] = useState("");
   const [packs, setPacks] = useState<Pack[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -15,29 +17,44 @@ export function PacksPage() {
       .then(res => res.status === 200 ? res.json() : [])
       .then(data => setPacks(data))
       .catch(err => console.error(err))
-    .finally(() => setLoading(false))
+      .finally(() => setLoading(false));
   }, []);
 
-  if (loading)
-    return <LoadingPage />
+  const handleQuery = () => {
+    setLoading(true);
+    fetch(`${BASE_API_URL}/packs?name=${query}`)
+      .then(res => res.status === 200 ? res.json() : [])
+      .then(data => setPacks(data))
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false));
+  };
 
   return (
     <>
       <div className="page-wrapper">
         <Header />
         <div className="page">
+          <div className="margin-top container search">
+            <input className="search-bar" onChange={(e) => setQuery(e.currentTarget.value)} name="query" id="query" type="text" placeholder="Type pack name..." />
+            <Button onClick={handleQuery} className="search-bar-submit" stretch={false} dim={false}>Search</Button>
+          </div>
           <div className="margin-top packs-menu container">
             <h2>Packs</h2>
-            <div className="packs">
-              <ul className="packs-list">
-                { 
-                  packs.length === 0 ?
-                    <h2>No packs currently exist</h2>
-                  :
-                    packs.map((pack) => <PackCard key={pack.id} pack={pack} /> )
-                }
-              </ul>
-            </div>
+            {
+              loading ?
+                <Spinner />
+              :
+                <div className="packs">
+                  <ul className="packs-list">
+                    { 
+                      !packs ?
+                        <h2>No packs found</h2>
+                        :
+                        packs.map((pack) => <PackCard key={pack.id} pack={pack} /> )
+                    }
+                  </ul>
+                </div>
+            }
           </div>
         </div>
         <Footer />
