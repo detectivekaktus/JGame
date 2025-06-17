e1a703bc3cb1b98acda1e2e58fe20765aac9f5f5
Artiom Astashonak <artiomastashonak@gmail.com>
2025-06-18 00:46:47 +0200

    Clean `player` and `room` database table on `SIGINT`.

--- cmd/jgame/main.go
+++ cmd/jgame/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/detectivekaktus/JGame/internal/config"
+	"github.com/detectivekaktus/JGame/internal/database"
 	"github.com/detectivekaktus/JGame/internal/router"
 )
 
 func main() {
 	r := router.NewRouter()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Println("Caught SIGINT signal. Cleaning up database before exiting...")
+
+		conn := database.GetConnection()
+		database.Execute(conn, "DELETE FROM rooms.player")
+		database.Execute(conn, "DELETE FROM rooms.room")
+		conn.Close(context.Background())
+
+		os.Exit(0)
+	}()
 	
 	log.Fatal(http.ListenAndServeTLS(":8080", config.AppConfig.SslCertPath, config.AppConfig.SslKeyPath, r))
 }
